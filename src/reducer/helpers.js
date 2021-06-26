a44e0ccbe2f8c795d0407070ef4d5e8f5beae74a
Ezequiel Romio <romio.ezequiel@gmail.com>
2021-06-26 11:22:22 -0300

    reducer/helpers created

---
+++ src/reducer/helpers.js
@@ -0,0 +1,60 @@
+const filterFileImportantData = (file) => {
+  return {
+    sha: file.sha,
+    filename: file.filename,
+    patch: file.patch,
+  }
+}
+
+export const filterCommitsImportantData = (commits) => {
+  const result = [];
+  // sha, commit {
+  //  author,
+  //  committer,
+  //  message,
+  //  comment_count  
+  //},
+  // html_url, comments_url,
+  // committer {login, avatar_url, html_url} 
+  for (const commit of commits) {
+    const filteredFields = {
+      sha: commit.sha,
+      htmlUrl: commit.html_url,
+      commentsUrl: commit.comments_url,
+      committer: {
+        login: commit.committer.login,
+        avatarUrl: commit.committer.avatar_url,
+        htmlUrl: commit.committer.html_url,
+      },
+      commit: {
+        committer: commit.commit.committer,
+        message: commit.commit.message,
+        commentCount: commit.commit.comment_count
+      }
+    };
+
+    result.push(filteredFields)
+  }
+  return result
+}
+
+export const filterCommitImportantData = (commit) => {
+  return {
+    sha: commit.sha,
+    commit: {
+      committer: commit.commit.committer,
+      message: commit.commit.message,
+      commentCount: commit.commit.comment_count,
+    },
+    htmlUrl: commit.html_url,
+    commentsUrl: commit.comments_url,
+    committer: {
+      login: commit.committer.login,
+      avatarUrl: commit.committer.avatar_url,
+      htmlUrl: commit.committer.html_url,
+    },
+    parents: commit.parents,
+    stats: commit.stats,
+    files: (commit.files.length > 0 && commit.files.map(file => filterFileImportantData(file))) || []
+  }
+}
