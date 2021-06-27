0da3a22da95cef2468feee97c77a99d797fecb9e
Ezequiel Romio <romio.ezequiel@gmail.com>
2021-06-27 12:40:18 -0300

    commit card reset, and image style moved to index.css

--- src/components/Commits.jsx
+++ src/components/Commits.jsx
@@ -2,22 +2,34 @@ import {Link} from "react-router-dom";
 
 import Commit from "./Commit";
 
+
+const displayCommits = (commits) => {
+  return commits.map(commit => {
+    return (
+      <Link to={`/details/${commit.sha}`}>
+        <Commit 
+          committer={commit.committer.login}
+          message={commit.commit.message}
+          date={commit.commit.committer.date}
+          avatar={commit.committer.avatarUrl}
+          committerAddress={commit.committer.htmlUrl}
+        />
+      </Link>
+    )
+  })
+}
+
 const Commits = ({commits}) => {
   return (
     <>
-      {commits.map(commit => {
-        return (
-          <Link to={`/details/${commit.sha}`}>
-            <Commit 
-              committer={commit.committer.login}
-              message={commit.commit.message}
-              date={commit.commit.committer.date}
-              avatar={commit.committer.avatar_url}
-              committerAddress={commit.committer.html_url}
-            />
-          </Link>
-        )
-      })}
+
+      {/* these validations are: 
+        1 - commits is not undefined;
+        2 - commits is an Array and has elements;
+        3 - commits elements have content to work with them;
+      */}
+
+      {commits && commits.length > 0 && Object.keys(commits[0]).length > 0 && displayCommits(commits)}
     </>
   )
 }
