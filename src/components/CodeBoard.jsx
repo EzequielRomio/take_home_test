362a7238baa1b5f24cc42fede390422806dd8706
Ezequiel Romio <romio.ezequiel@gmail.com>
2021-06-28 09:35:23 -0300

    detail fixed

--- src/components/CodeBoard.jsx
+++ src/components/CodeBoard.jsx
@@ -2,23 +2,26 @@
 const CodeBoard = ({filename, patch, status, additions, deletions}) => {
   return (
     <div>
-      <h4>{filename}</h4>
-      <h6>{status}</h6>
+      <h4>File: {filename}</h4>
+      <h6>Status: {status}</h6>
       <h6 style={{color: 'lightgreen'}}>{additions} additions</h6>
       <h6 style={{color: 'tomato'}}>{deletions} deletions</h6>
-      {patch.split('\n').map((line, ix) => {
-        
-        return (
-          <p key={ix}
-            className={'identation'} 
-            style={
-              line.startsWith('+') ? {backgroundColor: 'lightgreen'} : 
-              line.startsWith('-') ? {backgroundColor: 'tomato'} :
-              {backgroundColor: 'whitesmoke'}
-            }
-          >{!line.startsWith('@@') && line}</p>
-        )
-      })}
+      
+      <div style={{backgroundColor: 'whitesmoke'}}>
+        {patch.split('\n').map((line, ix) => {
+          return (
+            <p key={ix}
+              className={'identation'} 
+              style={
+                line.startsWith('+') ? {backgroundColor: 'lightgreen'} : 
+                line.startsWith('-') ? {backgroundColor: 'tomato'} :
+                {backgroundColor: 'none'}
+              }
+            >{!line.startsWith('@@') && line}</p>
+          )
+        })}
+      </div>
+      
     </div>
   )
 }
