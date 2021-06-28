b9367be7d86cda54b6442905ccc08a958674ecbe
Ezequiel Romio <romio.ezequiel@gmail.com>
2021-06-28 19:26:26 -0300

    Header component created

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import {Route} from 'react-router';
 
+import Header from './components/Header';
 import HomePage from './components/HomePage';
 import DetailsPage from './components/DetailsPage';
-
-import './App.css';
 
 function App() {
 
   return (
-    <>
+    <div className={'bg-dark text-white-50'}>
+      <Route path={'/'} component={Header} />
       <Route exact path={'/'} component={HomePage} />
       <Route exact path={'/details/:sha'} component={DetailsPage} />
-    </>
+    </div>
   );
 }
 
