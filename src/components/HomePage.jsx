4462a8802bf3a6f23a620cf553b60f3ca9158bb9
Ezequiel Romio <romio.ezequiel@gmail.com>
2021-06-29 16:12:01 -0300

    paginate added

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import Loading from "./Loading";
@@ -8,6 +8,8 @@ import Sort from "./Sort";
 import {getCommits} from "../actions/index.js";
 
 const HomePage = () => {
+  const [pageIndex, setPageIndex] = useState(0)
+
   const dispatch = useDispatch();
   const commits = useSelector((state) => state.commits)
 
@@ -17,16 +19,41 @@ const HomePage = () => {
     }
   })
 
+  const handleClick = (e, value) => {
+    e.preventDefault();
+    if (pageIndex + value < commits.length) {
+      setPageIndex(pageIndex + value <= 0 ? 0 : pageIndex + value)
+    }
+    window.scrollTo(0, 0)
+  }
+
   return (
-    <div>
-      <header>Commits History</header>
+    <div className={'container'} style={{height: '100%'}}>
       {!commits || !commits.length > 0 ? <Loading /> : 
-        <>
-          <Sort />
-          <Commits commits={commits} />
-        </>
-      } 
-    </div>
+        <div className={'row'}>
+          <div className={'col-8 mx-5 d-flex align-items-center justify-content-between'}>
+            <h3 >Commits History</h3>
+            <Sort />
+          </div>
+          <Commits commits={[...commits].splice(pageIndex, 6)} />
+
+          <ul className={'pagination justify-content-around'}>
+            {pageIndex >= 6 && 
+              <li className={'page-item'}>
+                <button className='btn btn-primary' onClick={(e) => handleClick(e, -6)}>Previous</button>
+              </li>
+            }
+            {pageIndex <= commits.length - 6 &&
+              <li className={'page-item'}>
+                <button className={'btn btn-primary'} onClick={(e) => handleClick(e, 6)}>Next</button>
+              </li>
+            }
+          </ul>
+
+        </div>
+      }
+
+    </div> 
   )
 }
 
