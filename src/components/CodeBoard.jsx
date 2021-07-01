30aaae113094f2547cfac042a6728467e5fe0dea
Ezequiel Romio <romio.ezequiel@gmail.com>
2021-07-01 19:47:28 -0300

    components refactorized

--- src/components/CodeBoard.jsx
+++ src/components/CodeBoard.jsx
@@ -1,52 +1,55 @@
 const parseFirstLine = (line) => {
-  const parsedLine = line.split('@@')[2];
-  return `   ${parsedLine}`;
-}
-
-const setClassname = (line) => {
-  if (line.startsWith('+')) return 'code addition-color addition-bg m-0 mx-3';
-  if (line.startsWith('-')) return 'code deletion-color deletion-bg m-0 mx-3';
-  return 'code white-70 m-0 mx-3'
-}
-
-const mapLinesIx = (txt) => {
-  const lines = txt.split('\n')
   //str to parse => @@ -37,4 +37,8 @@ 
-  //result will be -37; 37 
-  const aux = lines[0].split(' ')
-  const [left, right] = [parseInt(aux[1].split(',')[0]), parseInt(aux[2].split(',')[0])]
-  
-  let countLeft = Math.abs(left)
-  let countRight = right
-  return lines.map(line => {
-    const auxLeft = countLeft;
-    const auxRight = countRight;
-    if (!line.startsWith('-') && !line.startsWith('+')) {
-      countLeft++;
-      countRight++;
-      const letfStr = `${auxLeft >= 10 ? auxLeft : auxLeft === 0 ? '  ' : '0' + auxLeft}`
-      const rightStr =  `${auxRight >= 10 ? auxRight : auxRight === 0 ? '  ' : '0' + auxRight}`
-      return `${letfStr} ${rightStr}`
+  const initialNumbers = line.split('@@')[1];
+  const numbers = initialNumbers.split(' ')
+  const [leftNumber, rightNumber] = [Math.abs(parseInt(numbers[1].split(',')[0])), parseInt(numbers[2].split(',')[0])]
+
+  // these numbers indicate where the diff lines start
+  return [leftNumber, rightNumber]
+}
+
+
+const getLineFormat = (line, type, numberLeft, numberRight) => {
+  if (type === '-') return `${numberLeft < 10 ? '0' + numberLeft : numberLeft}    - ${line.slice(1, line.length)}`;
+  if (type === '+') return `   ${numberRight < 10 ? '0' + numberRight : numberRight} + ${line.slice(1, line.length)}`;
+  return `${numberLeft < 10 ? '0' + numberLeft : numberLeft} ${numberRight < 10 ? '0' + numberRight : numberRight}  ${line}`
+}
+
+
+const displayDiff = (patch) => {
+  const lines = patch.split('\n')
+  let [leftNumber, rightNumber] = parseFirstLine(lines[0])
+
+  return lines.map((line, ix) => {
+    const auxLeft = leftNumber;
+    const auxRight = rightNumber;
+    if (ix === lines.length - 1 && line.includes('\\ No newline at end of file')) {
+      return <p className={'code italic mt-1 mx-5'}>No newline at end of file</p>
     }
-    if (line.startsWith('-')) {
-      countLeft++;
-      return `${auxLeft < 10 ? '0' + auxLeft : auxLeft}   `
+
+    if (line.startsWith('@@')) {
+      return <p key={ix} className={'code text-white-50 m-0 mx-3 my-2'}>{` -  -  ${line}`}</p>
+    }
+
+    if (line.startsWith('-')) {      
+      leftNumber++;
+      return <p key={ix} className={'code deletion-color deletion-bg m-0 mx-3'}>{getLineFormat(line, '-', auxLeft, null)}</p>
     } 
-    countRight++;
-    return `   ${auxRight < 10 ? '0' + auxRight : auxRight}`
+
+    if (line.startsWith('+')) {
+      rightNumber++;
+      return <p key={ix} className={'code addition-color addition-bg m-0 mx-3'}>{getLineFormat(line, '+', null, auxRight)}</p>
+    }
+
+    leftNumber++;
+    rightNumber++;
+    return <p key={ix} className={'code white-70 m-0 mx-3'}>{getLineFormat(line, null, auxLeft, auxRight)}</p>
   })
-
+  
 }
 
-const setLineFormat = (line) => {
-  if (line.startsWith('-')) return ` - ${line.slice(1, line.length)}`;
-  if (line.startsWith('+')) return ` + ${line.slice(1, line.length)}`;
-  return `   ${line}`;
-}
 
 const CodeBoard = ({filename, patch, status, additions, deletions}) => {
-  const linesIndex = mapLinesIx(patch)
-
   return (
     <div id={filename} className={'card mb-3 border-secondary code-bg'}>
       <div className={'card-body border-bottom border-secondary mb-3'}>
@@ -57,14 +60,7 @@ const CodeBoard = ({filename, patch, status, additions, deletions}) => {
       </div>
 
       <div className={'over-flow'}>
-        {patch.split('\n').map((line, ix) => {
-          return (
-            <p key={ix} className={setClassname(line)} >              
-              {linesIndex[ix]}
-              {!line.startsWith('@@') ? setLineFormat(line) : parseFirstLine(line)}
-            </p>
-          )
-        })}
+        {displayDiff(patch)}
       </div>
       
     </div>
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -9,12 +9,28 @@ import RequestError from "./RequestError";
 
 import {getCommits} from "../actions/index.js";
 
+const displayPaginationButtons = (handleClick, pageIndex, commits) => {
+  return (
+    <ul className={'pagination justify-content-evenly'}>
+      {pageIndex >= 6 && 
+        <li className={'page-item'}>
+          <button className='btn btn-primary' onClick={(e) => handleClick(e, -6)}>Prev</button>
+        </li>
+      }
+      {pageIndex <= commits.length - 6 &&
+        <li className={'page-item'}>
+          <button className={'btn btn-primary'} onClick={(e) => handleClick(e, 6)}>Next</button>
+        </li>
+      }
+    </ul>
+  )
+}
+
 const HomePage = () => {
   const [pageIndex, setPageIndex] = useState(0)
-
-  const dispatch = useDispatch();
   const commits = useSelector((state) => state.commits)
   const requestError = useSelector((state) => state.requestError)
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (commits && commits.length === 0 && !requestError) {
@@ -37,28 +53,18 @@ const HomePage = () => {
         {requestError ? <RequestError error={'getCommits'}/> :
           !commits || !commits.length > 0 ? <Loading /> : 
             <div className={'row'}>
+              
               <div className={'w-75 col-10 col-xs-8 col-sm-6 col-lg-4 mx-5 d-flex align-items-center justify-content-between'}>
                 <h3 >Commits History</h3>
                 <Sort />
               </div>
+
               <Commits commits={[...commits].splice(pageIndex, 6)} />
 
-              <ul className={'pagination justify-content-evenly'}>
-                {pageIndex >= 6 && 
-                  <li className={'page-item'}>
-                    <button className='btn btn-primary' onClick={(e) => handleClick(e, -6)}>Prev</button>
-                  </li>
-                }
-                {pageIndex <= commits.length - 6 &&
-                  <li className={'page-item'}>
-                    <button className={'btn btn-primary'} onClick={(e) => handleClick(e, 6)}>Next</button>
-                  </li>
-                }
-              </ul>
+              {displayPaginationButtons(handleClick, pageIndex, commits)}
 
             </div>
         }
-
       </div> 
     </>
   )
