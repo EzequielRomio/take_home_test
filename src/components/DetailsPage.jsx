656d655ac9ba2888465373aa4fc0281422f090a7
Ezequiel Romio <romio.ezequiel@gmail.com>
2021-07-01 16:27:37 -0300

    React-Router Switch implemented

--- src/components/DetailsPage.jsx
+++ src/components/DetailsPage.jsx
@@ -2,10 +2,13 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
 
+import Header from "./Header";
 import Commit from "./Commit";
 import CodeBoard from "./CodeBoard";
 import Loading from "./Loading";
 import RequestError from "./RequestError";
+import FileIcon from "./assets/FileIcon";
+import GithubIcon from "./assets/GithubIcon";
 
 import { getCommitDetail, resetError } from "../actions/index.js";
 
@@ -30,17 +33,7 @@ const displayDetails = (commit) => {
             className={'list-group-item bg-hover'}
             key={file.filename}
           >
-            <svg
-              xmlns={"http://www.w3.org/2000/svg"}
-              width="16" height="16"
-              fill="currentColor"
-              className={"bi bi-file-diff display-none"}
-              viewBox="0 0 16 16"
-            >
-              <path d="M8 4a.5.5 0 0 1 .5.5V6H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V7H6a.5.5 0 0 1 0-1h1.5V4.5A.5.5 0 0 1 8 4zm-2.5 6.5A.5.5 0 0 1 6 10h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5z" />
-              <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1z" />
-            </svg>
-
+            <FileIcon />
             <span className={'mx-3'}>
               {file.filename}
             </span>
@@ -72,9 +65,7 @@ const displayDetails = (commit) => {
           
         >
           Click here to Check this commit at github.com
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-github mx-2" viewBox="0 0 16 16">
-            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-          </svg>
+          <GithubIcon />
         </a>
       </span>
 
@@ -102,24 +93,27 @@ const DetailsPage = ({ match }) => {
   }
 
   return (
-    <div className={'container mb-5'}>
-      {requestError ? 
-        <>
-          <RequestError error={'getCommit'} />
-          <button className={'btn btn-outline-primary text-decoration-none mx-5'} onClick={handleClick}>
-            Go back to home  
-          </button> 
-        </>
-        :
-        <> 
-          <div className={'col-12 col-xs-10 col-sm-8 col-lg-6 mx-auto d-flex align-items-center justify-content-end'}>
-            <h3 className={'mt-4 mb-4 mx-5'}>Commit Details</h3> 
-            <Link to={'/'} className={'btn btn-outline-primary text-decoration-none'}>Go back to Home</Link>
-          </div>
-          {commit && Object.keys(commit).length > 0 ? displayDetails(commit) : <Loading />}
-        </>
-      }
-    </div>
+    <>
+    <Header />
+      <div className={'container mb-5'}>
+        {requestError ? 
+          <>
+            <RequestError error={'getCommit'} />
+            <button className={'btn btn-outline-primary text-decoration-none mx-5'} onClick={handleClick}>
+              Go back to home  
+            </button> 
+          </>
+          :
+          <> 
+            <div className={'col-12 col-xs-10 col-sm-8 col-lg-6 mx-auto d-flex align-items-center justify-content-end'}>
+              <h3 className={'mt-4 mb-4 mx-5'}>Commit Details</h3> 
+              <Link to={'/'} className={'btn btn-outline-primary text-decoration-none'}>Go back to Home</Link>
+            </div>
+            {commit && Object.keys(commit).length > 0 ? displayDetails(commit) : <Loading />}
+          </>
+        }
+      </div>
+    </>
   )
 }
 
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+import Header from "./Header";
 import Loading from "./Loading";
 import Commits from "./Commits";
 import Sort from "./Sort";
@@ -30,33 +31,36 @@ const HomePage = () => {
   }
 
   return (
-    <div className={'container h-100'}>
-      {requestError ? <RequestError error={'getCommits'}/> :
-        !commits || !commits.length > 0 ? <Loading /> : 
-          <div className={'row'}>
-            <div className={'w-75 col-10 col-xs-8 col-sm-6 col-lg-4 mx-5 d-flex align-items-center justify-content-between'}>
-              <h3 >Commits History</h3>
-              <Sort />
+    <>
+      <Header />
+      <div className={'container h-100'}>
+        {requestError ? <RequestError error={'getCommits'}/> :
+          !commits || !commits.length > 0 ? <Loading /> : 
+            <div className={'row'}>
+              <div className={'w-75 col-10 col-xs-8 col-sm-6 col-lg-4 mx-5 d-flex align-items-center justify-content-between'}>
+                <h3 >Commits History</h3>
+                <Sort />
+              </div>
+              <Commits commits={[...commits].splice(pageIndex, 6)} />
+
+              <ul className={'pagination justify-content-evenly'}>
+                {pageIndex >= 6 && 
+                  <li className={'page-item'}>
+                    <button className='btn btn-primary' onClick={(e) => handleClick(e, -6)}>Prev</button>
+                  </li>
+                }
+                {pageIndex <= commits.length - 6 &&
+                  <li className={'page-item'}>
+                    <button className={'btn btn-primary'} onClick={(e) => handleClick(e, 6)}>Next</button>
+                  </li>
+                }
+              </ul>
+
             </div>
-            <Commits commits={[...commits].splice(pageIndex, 6)} />
+        }
 
-            <ul className={'pagination justify-content-evenly'}>
-              {pageIndex >= 6 && 
-                <li className={'page-item'}>
-                  <button className='btn btn-primary' onClick={(e) => handleClick(e, -6)}>Prev</button>
-                </li>
-              }
-              {pageIndex <= commits.length - 6 &&
-                <li className={'page-item'}>
-                  <button className={'btn btn-primary'} onClick={(e) => handleClick(e, 6)}>Next</button>
-                </li>
-              }
-            </ul>
-
-          </div>
-      }
-
-    </div> 
+      </div> 
+    </>
   )
 }
 
